9ea7134140ce3fd68afe7934df80c479fa93703a
ngan <vutran@sheridancollege.ca>
2024-08-08 15:50:37 -0400

    Turn text-gray-300 into reusable utility class

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -4,17 +4,17 @@ export function HeaderBar() {
   return (
     <header className="flex items-end justify-between py-3 px-4">
       <button
-        className="text-gray-300 font-extrabold text-2xl hover:cursor-pointer"
+        className="text-color font-extrabold text-2xl hover:cursor-pointer"
         onClick={(e) => console.log("home page")}
       >
         David Vu
       </button>
-      <h2 className="text-gray-300 mr-auto ml-4">Software Developer</h2>
+      <h2 className="text-color mr-auto ml-4">Software Developer</h2>
       <button
         className="relative top-2 hover:cursor-pointer"
         onClick={(e) => console.log("open menu")}
       >
-        <Bars3Icon className="text-gray-300 h-10 w-10" />
+        <Bars3Icon className="text-color h-10 w-10" />
       </button>
     </header>
   )
