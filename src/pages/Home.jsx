d37f038aea74bcfe2c91455b3a75b182adf61319
ngan <vutran@sheridancollege.ca>
2024-08-11 15:53:44 -0400

    Text in home page is done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ export function Home() {
       <h1 className="text-4xl">Hello</h1>
       <h2 className="text-xl mb-1 mt-4 tracking-wide">A Bit About Me</h2>
       <p className="tracking-widest leading-6">
-        I'm a paragraph. Click here to add your own text and edit me. I’m a
-        great place for you to tell a story and let your users know a little
-        more about you.
+        I'm a passionate software developer specializing in creating dynamic and
+        responsive web applications. Explore my portfolio to see how I turn
+        ideas into impactful digital experiences.
       </p>
       <div className="mt-4 flex items-center justify-between gap-x-2">
         <button className="w-28 sm:w-32 h-28 sm:h-32 rounded-full border-gray-300 border-4 font-extrabold hover:bg-gray-300 hover:text-gray-900 transition-all duration-300">
