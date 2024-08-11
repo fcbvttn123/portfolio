25ac071fc9b0bf0bae51a0edcfd889cd2168b7d7
ngan <vutran@sheridancollege.ca>
2024-08-11 16:22:56 -0400

    Footer Section is done on mobile design

---
+++ src/components/FooterSection.jsx
@@ -0,0 +1,28 @@
+import { CiLinkedin } from "react-icons/ci"
+import { FaGithub } from "react-icons/fa"
+
+export function FooterSection() {
+  return (
+    <footer className="text-color mt-14 pt-7 -mx-4 px-4 border-t-2 border-gray-300">
+      <div>
+        <h1 className="font-bold text-xl mb-1">Phone</h1>
+        <a href="tel:+14372136600">437-213-6600</a>
+      </div>
+      <div className="my-4">
+        <h1 className="font-bold text-xl mb-1">Email</h1>
+        <a href="mailto:vutran@sheridancollege.ca">vutran@sheridancollege.ca</a>
+      </div>
+      <div>
+        <h1 className="font-bold text-xl mb-1">Follow me</h1>
+        <div className="flex items-center justify-start gap-x-2">
+          <a href="https://www.linkedin.com/in/davidvttn/" target="_blank">
+            <CiLinkedin size={30} />
+          </a>
+          <a href="https://github.com/fcbvttn123" target="_blank">
+            <FaGithub size={30} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom"
 import { HeaderBar } from "./HeaderBar"
 import { useState } from "react"
 import { MobileMenu } from "./MobileMenu"
+import { FooterSection } from "./FooterSection"
 
 export default function Layout() {
   const [mobileOpen, setMobileOpen] = useState(false)
@@ -10,6 +11,7 @@ export default function Layout() {
       <HeaderBar setMobileOpen={setMobileOpen} />
       <MobileMenu opened={mobileOpen} setMobileOpen={setMobileOpen} />
       <Outlet />
+      <FooterSection />
     </div>
   )
 }
