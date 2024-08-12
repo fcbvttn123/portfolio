34c5317e5a99e197fd8639611c26c3dba35cf01e
ngan <vutran@sheridancollege.ca>
2024-08-12 15:22:01 -0400

    Mobile Menu is finished

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,11 +1,47 @@
 import Drawer from "@mui/material/Drawer"
 import Box from "@mui/material/Box"
+import { List, ListItem, ListItemButton } from "@mui/material"
+import { DocumentIcon } from "@heroicons/react/20/solid"
+import { ComputerDesktopIcon } from "@heroicons/react/20/solid"
+import { EnvelopeIcon } from "@heroicons/react/20/solid"
+import { Link } from "react-router-dom"
 
 export function MobileMenu({ opened, setMobileOpen }) {
+  const links = [
+    {
+      path: "/resume",
+      icon: <DocumentIcon className="text-color w-8 h-8" />,
+      text: "Resume",
+    },
+    {
+      path: "/projects",
+      icon: <ComputerDesktopIcon className="text-color w-8 h-8" />,
+      text: "Projects",
+    },
+    {
+      path: "/contact",
+      icon: <EnvelopeIcon className="text-color w-8 h-8" />,
+      text: "Contact",
+    },
+  ]
   return (
     <Drawer open={opened} onClose={() => setMobileOpen(false)}>
-      <Box sx={{ width: 250 }} role="presentation">
-        {/* {drawer} */}
+      <Box
+        sx={{ width: 250, height: "100%", backgroundColor: "rgb(31 41 55)" }}
+        role="presentation"
+      >
+        <List>
+          {links.map((e, i) => (
+            <ListItem key={e.text + i} disablePadding>
+              <Link to={e.path}>
+                <ListItemButton>
+                  {e.icon}
+                  <p className="text-color ml-4 font-bold text-lg">{e.text}</p>
+                </ListItemButton>
+              </Link>
+            </ListItem>
+          ))}
+        </List>
       </Box>
     </Drawer>
   )
