a57149c3046f22dd3d04083109d7448c022c8169
ngan <vutran@sheridancollege.ca>
2024-08-13 09:11:45 -0400

    RWD is done for the Header Bar

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -10,8 +10,13 @@ export function HeaderBar({ setMobileOpen }) {
         David Vu
       </button>
       <h2 className="text-color mr-auto ml-4">Software Developer</h2>
+      <div className="text-color hidden md:flex md:items-center md:justify-between md:gap-x-4">
+        <button>Resume</button>
+        <button>Projects</button>
+        <button>Contact</button>
+      </div>
       <button
-        className="relative top-2 hover:cursor-pointer"
+        className="relative top-2 hover:cursor-pointer md:hidden"
         onClick={(e) => setMobileOpen(true)}
       >
         <Bars3Icon className="text-color h-10 w-10" />
