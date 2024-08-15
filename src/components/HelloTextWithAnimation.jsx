fa0b4ecd0cae334901bcba2f5cb421adccba2549
ngan <vutran@sheridancollege.ca>
2024-08-15 16:51:43 -0400

    Hello Text Animation is finished

---
+++ src/components/HelloTextWithAnimation.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+
+const textArray = [
+  "Hello! I'm David Vu",
+  "I'm a Developer",
+  "I'm a Footballer",
+  "Nice to meet you!",
+]
+
+export function HelloTextWithAnimation() {
+  const [index, setIndex] = useState(0)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => {
+        if (prev >= textArray.length - 1) {
+          return 0
+        } else {
+          return prev + 1
+        }
+      })
+    }, 3000)
+    return () => clearInterval(interval)
+  }, [])
+  return (
+    <h1 className="text-4xl lg:text-5xl relative overflow-hidden hello-text-at-home-page">
+      {textArray[index]}
+    </h1>
+  )
+}
