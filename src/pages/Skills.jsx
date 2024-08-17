dcf0b4da3bd95ccc690ceb66611bd8d0a75edc0e
ngan <vutran@sheridancollege.ca>
2024-08-17 09:49:04 -0400

    Skill Card is created

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,12 @@
 export function Skills() {
   return (
-    <section className="text-color">
-      <h1>Skills</h1>
+    <section className="text-color mx-auto max-w-4xl grid justify-items-center grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="skill-card-glass" data-text="HTML"></div>
+      <div className="skill-card-glass" data-text="HTML"></div>
+      <div className="skill-card-glass" data-text="HTML"></div>
+      <div className="skill-card-glass" data-text="HTML"></div>
+      <div className="skill-card-glass" data-text="HTML"></div>
+      <div className="skill-card-glass" data-text="HTML"></div>
     </section>
   )
 }
