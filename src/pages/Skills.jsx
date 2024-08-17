7691abcf91364c30346ca876e1530d9855b4e76d
ngan <vutran@sheridancollege.ca>
2024-08-17 10:51:10 -0400

    Skill JSON file is done

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -2,13 +2,24 @@ import { skills } from "../data/skills"
 
 export function Skills() {
   return (
-    <section className="text-color mx-auto max-w-4xl grid justify-items-center grid-cols-2 md:grid-cols-4 gap-4">
-      {skills.map((e) => (
-        <div key={e.text} className="skill-card-glass" data-text={e.text}>
-          <img
-            className="w-[60%] h-[60%] mt-5 rounded-full"
-            src={`./images/${e.imageName}`}
-          />
+    <section className="text-color">
+      {skills.map((topic) => (
+        <div className="row-span-full mb-8">
+          <h1 className="text-xl mb-4">{topic.topic}</h1>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+            {topic.list.map((item) => (
+              <div
+                key={item.text}
+                className="skill-card-glass"
+                data-text={item.text}
+              >
+                <img
+                  className="w-[60%] h-[60%] mt-5 rounded-full"
+                  src={`./images/${item.imageName}`}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       ))}
     </section>
