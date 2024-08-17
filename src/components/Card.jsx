3677f4f5843ea5305a7699c7473e3d8da91082ba
ngan <vutran@sheridancollege.ca>
2024-08-17 08:46:06 -0400

    Add tech stack to each project card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,41 @@
 import { FaGithub } from "react-icons/fa"
+import { FaReact } from "react-icons/fa"
+import { FaNodeJs } from "react-icons/fa"
+import { DiMongodb } from "react-icons/di"
+import { DiMysql } from "react-icons/di"
 
 export function Card() {
   return (
-    <div className="card-container">
-      {/* <button
+    <div>
+      <div className="card-container">
+        {/* <button
         data-text="Tech Stack"
         style={{ "--r": "-15" }}
         className="card-glass"
         onClick={(e) => console.log("Tech Stack")}
       ></button> */}
-      <button
-        data-text="GitHub"
-        // style={{ "--r": "5" }}
-        style={{ "--r": "-15" }}
-        className="card-glass"
-        onClick={(e) => console.log("GitHub")}
-      >
-        <FaGithub className="text-3xl" />
-      </button>
-      <button
-        data-text="Demo"
-        style={{ "--r": "25" }}
-        className="card-glass"
-        onClick={(e) => console.log("Demo")}
-      ></button>
+        <button
+          data-text="GitHub"
+          // style={{ "--r": "5" }}
+          style={{ "--r": "-15" }}
+          className="card-glass"
+          onClick={(e) => console.log("GitHub")}
+        >
+          <FaGithub className="text-3xl" />
+        </button>
+        <button
+          data-text="Demo"
+          style={{ "--r": "25" }}
+          className="card-glass"
+          onClick={(e) => console.log("Demo")}
+        ></button>
+      </div>
+      <div className="mt-8 flex items-center justify-center gap-2">
+        <FaReact className="text-3xl text-color" />
+        <FaNodeJs className="text-3xl text-color" />
+        <DiMongodb className="text-3xl text-color" />
+        <DiMysql className="text-3xl text-color" />
+      </div>
     </div>
   )
 }
