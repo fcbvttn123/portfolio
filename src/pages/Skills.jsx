9552b90dc40abc67daa63c5dec9076a2c1f548d8
ngan <vutran@sheridancollege.ca>
2024-08-17 10:36:21 -0400

    Align the image of each skill card. Working on JSON file for skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,12 +1,16 @@
+import { skills } from "../data/skills"
+
 export function Skills() {
   return (
-    <section className="text-color mx-auto max-w-4xl grid justify-items-center grid-cols-2 md:grid-cols-3 gap-4">
-      <div className="skill-card-glass" data-text="HTML"></div>
-      <div className="skill-card-glass" data-text="HTML"></div>
-      <div className="skill-card-glass" data-text="HTML"></div>
-      <div className="skill-card-glass" data-text="HTML"></div>
-      <div className="skill-card-glass" data-text="HTML"></div>
-      <div className="skill-card-glass" data-text="HTML"></div>
+    <section className="text-color mx-auto max-w-4xl grid justify-items-center grid-cols-2 md:grid-cols-4 gap-4">
+      {skills.map((e) => (
+        <div key={e.text} className="skill-card-glass" data-text={e.text}>
+          <img
+            className="w-[60%] h-[60%] mt-5 rounded-full"
+            src={`./images/${e.imageName}`}
+          />
+        </div>
+      ))}
     </section>
   )
 }
