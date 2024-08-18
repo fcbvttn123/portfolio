643669fa7b8a593345f5bc3ec6e6935e105ee0c1
ngan <vutran@sheridancollege.ca>
2024-08-17 20:08:12 -0400

    Work Experience section is finished

--- src/pages/WorkExperiences.jsx
+++ src/pages/WorkExperiences.jsx
@@ -1,7 +1,110 @@
 export function WorkExperiences() {
   return (
-    <section className="text-color">
-      <h1>Work Experiences</h1>
+    <section>
+      {/* <div className="company-card">
+        <div className="bg"></div>
+        <div className="blob"></div>
+      </div> */}
+      {/* Job 1 */}
+      <div className="mb-14">
+        <h1 className="text-color text-4xl mb-8 pb-3 max-w-xl border-b-2">
+          Technical Analyst - Intern
+        </h1>
+        <div className="work-details">
+          <div className="circles">
+            <div className="c"></div>
+            <div className="c"></div>
+            <div className="c"></div>
+          </div>
+          <code>
+            <p className="mb-4">Company: Ministry of Transportation</p>
+            <p className="mb-4">Period: 01/2023 - 05/2023</p>
+            <p className="mb-4">What I've done:</p>
+            <ul>
+              <li>
+                Effective communication to train on the use of tools, resolve
+                technical issues, and present deliverables and status.
+              </li>
+              <li>
+                Supporting creation of I&IT architecture artifacts based on
+                Ontario Public Service.
+              </li>
+              <li>
+                Provided technical support in the development of SharePoint and
+                document management repositories.
+              </li>
+              <li>
+                Participated in IT security activities: security assessments,
+                development, and management of security deliverables.
+              </li>
+            </ul>
+          </code>
+        </div>
+      </div>
+      {/* Job 2 */}
+      <div className="mb-14">
+        <h1 className="text-color text-4xl mb-8 pb-3 max-w-xl border-b-2">
+          Support Technologist - Intern
+        </h1>
+        <div className="work-details">
+          <div className="circles">
+            <div className="c"></div>
+            <div className="c"></div>
+            <div className="c"></div>
+          </div>
+          <code>
+            <p className="mb-4">Company: Sheridan College</p>
+            <p className="mb-4">Period: 09/2023 - 05/2024</p>
+            <p className="mb-4">What I've done:</p>
+            <ul>
+              <li>
+                Provided Classroom support to clients using the mobile labs,
+                smart classrooms, and Computer labs.
+              </li>
+              <li>
+                Resolved inquiries regarding desktop and laptop computers,
+                operating systems, PC applications software and voice/data/video
+                networks.
+              </li>
+              <li>
+                Working in IT Service Desk with troubleshooting knowledge of
+                business hardware, software, and solutions.
+              </li>
+            </ul>
+          </code>
+        </div>
+      </div>
+      {/* Job 3 */}
+      <div className="mb-14">
+        <h1 className="text-color text-4xl mb-8 pb-3 max-w-xl border-b-2">
+          React Developer - Intern
+        </h1>
+        <div className="work-details">
+          <div className="circles">
+            <div className="c"></div>
+            <div className="c"></div>
+            <div className="c"></div>
+          </div>
+          <code>
+            <p className="mb-4">Company: SkillSquirrel</p>
+            <p className="mb-4">Period: 05/2024 - 09/2024</p>
+            <p className="mb-4">What I've done:</p>
+            <ul>
+              <li>
+                Using React and Material UI for developing website interfaces.
+              </li>
+              <li>
+                Using Jira and Github to create branches and make pull/push
+                requests.
+              </li>
+              <li>Working with various APIs and third-party tools.</li>
+              <li>
+                Applying plain JavaScript for website keyboard navigation.
+              </li>
+            </ul>
+          </code>
+        </div>
+      </div>
     </section>
   )
 }
