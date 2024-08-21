be2a2da12bfde27f1217f11388e443dd6f68a089
ngan <vutran@sheridancollege.ca>
2024-08-20 23:25:11 -0400

    Update again the css class name for text color of each step. Stepper is done

--- src/pages/StepperComponent.jsx
+++ src/pages/StepperComponent.jsx
@@ -6,26 +6,29 @@ import { useState } from "react"
 
 const steps = [
   {
-    label: "Select campaign settings",
+    label: "Technical Analyst",
+    imageName: "first-intern-job-icon.png",
     description: `For each ad campaign that you create, you can control how much you're willing to spend on clicks and conversions, which networks and geographical locations you want your ads to show on, and more.`,
   },
   {
-    label: "Create an ad group",
+    label: "Support Technologist",
+    imageName: "sheridan.jpeg",
     description:
       "An ad group contains one or more ads which target a shared set of keywords.",
   },
   {
-    label: "Create an ad",
+    label: "React Developer",
+    imageName: "sksq.jpeg",
     description: `Try out different ad text to see what brings in the most customers, and learn how to enhance your ads using features like ad extensions. If you run into any problems with your ads, find out how to tell if they're running and how to resolve approval issues.`,
   },
 ]
 
-function StepLabelIcon() {
+function StepLabelIcon(imageName) {
   return (
     <div className="w-8 aspect-square rounded-full">
       <img
-        src="./images/first-intern-job-icon.png"
-        className="w-full h-full rounded-full"
+        src={`./images/${imageName}`}
+        className="w-full h-full rounded-full object-cover"
       />
     </div>
   )
@@ -43,11 +46,11 @@ export function StepperComponent() {
             color: "rgba(209, 213, 219, .4)",
             cursor: "pointer",
           },
-          "& .Mui-completed": {
+          "& .MuiStepLabel-label.Mui-completed": {
             color: "rgba(209, 213, 219, .4)",
           },
-          "& .Mui-active": {
-            color: "rgba(209, 213, 219, 1.0)",
+          "& .MuiStepLabel-label.Mui-active": {
+            color: "rgba(209, 213, 219, 1)",
           },
           "& .MuiStepConnector-line": {
             minHeight: "60px",
@@ -57,7 +60,9 @@ export function StepperComponent() {
         {steps.map((step, index) => (
           <Step>
             <button className="ml-[-3px]" onClick={(e) => setActiveStep(index)}>
-              <StepLabel StepIconComponent={StepLabelIcon}>
+              <StepLabel
+                StepIconComponent={() => StepLabelIcon(step.imageName)}
+              >
                 {step.label}
               </StepLabel>
             </button>
