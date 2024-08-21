584eaba6e02f754a0f23093ea3c2a20add274662
ngan <vutran@sheridancollege.ca>
2024-08-21 18:29:19 -0400

    Create Mac Screen component

---
+++ src/components/MacScreen.jsx
@@ -0,0 +1,14 @@
+export function MacScreen({ children, className }) {
+  return (
+    <div
+      className={`bg-gray-300 overflow-hidden px-3 py-1 text-gray-900 border-2 ${className}`}
+    >
+      <div className="h-[30px] w-[20%] flex items-center gap-[8px]">
+        <div className="w-[9px] aspect-square rounded-full bg-red-600"></div>
+        <div className="w-[9px] aspect-square rounded-full bg-yellow-600"></div>
+        <div className="w-[9px] aspect-square rounded-full bg-green-700"></div>
+      </div>
+      <code>{children}</code>
+    </div>
+  )
+}
--- src/pages/StepperPage.jsx
+++ src/pages/StepperPage.jsx
@@ -1,14 +1,33 @@
 import { useState } from "react"
 import { StepperComponent } from "../components/StepperComponent"
 import { workExperiences } from "../data/work-experiences"
+import { MacScreen } from "../components/MacScreen"
 
 export function StepperPage() {
   const [activeStep, setActiveStep] = useState(0)
   return (
-    <StepperComponent
-      steps={workExperiences}
-      activeStep={activeStep}
-      setActiveStep={setActiveStep}
-    />
+    <div className="flex justify-between items-stretch gap-x-32">
+      <StepperComponent
+        steps={workExperiences}
+        activeStep={activeStep}
+        setActiveStep={setActiveStep}
+      />
+      <MacScreen className="flex-1">
+        <p className="mb-4">Company: SkillSquirrel</p>
+        <p className="mb-4">Period: 05/2024 - 09/2024</p>
+        <p className="mb-4">What I've done:</p>
+        <ul>
+          <li>
+            Using React and Material UI for developing website interfaces.
+          </li>
+          <li>
+            Using Jira and Github to create branches and make pull/push
+            requests.
+          </li>
+          <li>Working with various APIs and third-party tools.</li>
+          <li>Applying plain JavaScript for website keyboard navigation.</li>
+        </ul>
+      </MacScreen>
+    </div>
   )
 }
