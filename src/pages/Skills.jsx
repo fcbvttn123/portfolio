462515986318892ecc157f1af4fdc727720acfff
ngan <vutran@sheridancollege.ca>
2024-08-26 10:06:10 -0400

    Add skill card to skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -5,7 +5,7 @@ import { skills } from "../data/skills"
 export function Skills() {
   const [activeStep, setActiveStep] = useState(0)
   return (
-    <section className="text-color">
+    <section className="text-color flex justify-between items-start gap-x-24">
       <StepperComponent
         steps={skills}
         activeStep={activeStep}
@@ -13,25 +13,20 @@ export function Skills() {
         showStepContent={false}
         stepLabelRounded={false}
       />
-      {/* {skills.map((topic, index) => (
-        <div key={index} className="row-span-full mb-8">
-          <h1 className="text-xl mb-4">{topic.topic}</h1>
-          <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
-            {topic.list.map((item) => (
-              <div
-                key={item.text}
-                className="skill-card-glass"
-                data-text={item.text}
-              >
-                <img
-                  className="w-[60%] h-[60%] mt-5 rounded-full"
-                  src={`./images/${item.imageName}`}
-                />
-              </div>
-            ))}
+      <div className="grid grid-cols-4 gap-4 flex-1">
+        {skills[activeStep].list.map((item, index) => (
+          <div
+            key={item.text}
+            className="skill-card-glass"
+            data-text={item.text}
+          >
+            <img
+              className="w-[50%] h-[50%] mt-5 rounded-full"
+              src={`./images/${item.imageName}`}
+            />
           </div>
-        </div>
-      ))} */}
+        ))}
+      </div>
     </section>
   )
 }
