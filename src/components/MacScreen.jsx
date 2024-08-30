5accb8db47ec6c35dc7ea4af6d8af266c727c6a8
ngan <vutran@sheridancollege.ca>
2024-08-29 22:37:26 -0400

    Make border of mac screen rounded

--- src/components/MacScreen.jsx
+++ src/components/MacScreen.jsx
@@ -1,7 +1,7 @@
 export function MacScreen({ children, className }) {
   return (
     <div
-      className={`bg-[#fff2] overflow-hidden px-3 py-1 text-gray-300 h-full ${className}`}
+      className={`bg-[#fff2] overflow-hidden px-5 py-2 text-gray-300 h-full rounded-xl ${className}`}
     >
       <div className="h-[30px] w-[20%] flex items-center gap-[8px]">
         <div className="w-[9px] aspect-square rounded-full bg-red-600"></div>
