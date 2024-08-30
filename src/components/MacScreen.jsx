46d1510bed197ee87a57d1c9c4ba2212dc0dc5ea
ngan <vutran@sheridancollege.ca>
2024-08-30 18:36:53 -0400

    Edit padding top and bottom for the Mac Screen component with the same number

--- src/components/MacScreen.jsx
+++ src/components/MacScreen.jsx
@@ -1,9 +1,9 @@
 export function MacScreen({ children, className }) {
   return (
     <div
-      className={`bg-[#fff2] overflow-hidden px-5 py-2 text-gray-300 h-full rounded-xl ${className}`}
+      className={`bg-[#fff2] overflow-hidden px-5 py-4 text-gray-300 h-full rounded-xl ${className}`}
     >
-      <div className="h-[30px] w-[20%] flex items-center gap-[8px]">
+      <div className="w-[20%] flex items-center gap-[8px]">
         <div className="w-[9px] aspect-square rounded-full bg-red-600"></div>
         <div className="w-[9px] aspect-square rounded-full bg-yellow-600"></div>
         <div className="w-[9px] aspect-square rounded-full bg-green-700"></div>
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,13 +1,8 @@
-export function ProjectCard({ className }) {
+import { MacScreen } from "../components/MacScreen"
+
+export function ProjectCard() {
   return (
-    <div
-      className={`bg-[#fff2] w-[450px] h-[340px] overflow-hidden px-5 py-2 text-gray-300 rounded-xl ${className}`}
-    >
-      <div className="h-[30px] w-[20%] flex items-center gap-[8px]">
-        <div className="w-[9px] aspect-square rounded-full bg-red-600"></div>
-        <div className="w-[9px] aspect-square rounded-full bg-yellow-600"></div>
-        <div className="w-[9px] aspect-square rounded-full bg-green-700"></div>
-      </div>
+    <MacScreen className="max-w-[450px]">
       <div className="mt-4 flex gap-x-4">
         <div>
           <div className="w-[180px] h-[180px] bg-gray-900 rounded-xl"></div>
@@ -15,6 +10,6 @@ export function ProjectCard({ className }) {
         </div>
         <div className="h-[254px] w-[100%] bg-gray-900 rounded-xl"></div>
       </div>
-    </div>
+    </MacScreen>
   )
 }
