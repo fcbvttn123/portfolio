5797a896bb6314eba5122d30d9c8b762f9201ce5
ngan <vutran@sheridancollege.ca>
2024-08-31 10:20:31 -0400

    Update components into the correct files

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,41 +1,42 @@
 import { MacScreen } from "../components/MacScreen"
 import { FaGithub } from "react-icons/fa"
 import { AiOutlineGlobal } from "react-icons/ai"
-import { projectsArr } from "../data/projects"
 
-export function ProjectCard() {
+export function ProjectCard({
+  projectName,
+  projectImageName,
+  techStack,
+  gitHubUrl,
+  demoUrl,
+}) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-y-10">
-      {projectsArr.map((item) => (
-        <MacScreen className="max-w-[530px] w-full cursor-pointer transition-all duration-500 hover:scale-105">
-          <div className="grid grid-cols-2 grid-rows-2 gap-3 mt-5">
-            <div
-              className="bg-gray-900 rounded-xl flex flex-wrap gap-x-2 justify-between overflow-hidden bg-cover bg-center"
-              style={{
-                backgroundImage: `url("./projects-images/${item.img_name}")`,
-              }}
-            ></div>
-            <div className="bg-gray-900 rounded-xl row-span-2 p-3 flex flex-wrap items-start content-start gap-3">
-              {item.tech_stack.map((techName) => (
-                <p className="border-2 border-gray-300 rounded-2xl px-2 py-1 bg-[#fff2]">
-                  {techName}
-                </p>
-              ))}
-            </div>
-            <div className="bg-gray-900 rounded-xl px-4 py-3">
-              <p className="text-lg font-extrabold mb-4">{item.project_name}</p>
-              <div className="flex items-center gap-x-2 mt-2">
-                <a href={item.github} target="_blank">
-                  <FaGithub className="text-3xl cursor-pointer transition-all duration-500 hover:scale-110" />
-                </a>
-                <a href={item.url} target="_blank">
-                  <AiOutlineGlobal className="text-3xl cursor-pointer transition-all duration-500 hover:scale-110" />
-                </a>
-              </div>
-            </div>
+    <MacScreen className="max-w-[530px] w-full cursor-pointer transition-all duration-500 hover:scale-105">
+      <div className="grid grid-cols-2 grid-rows-2 gap-3 mt-5">
+        <div
+          className="bg-gray-900 rounded-xl flex flex-wrap gap-x-2 justify-between overflow-hidden bg-cover bg-center"
+          style={{
+            backgroundImage: `url("./projects-images/${projectImageName}")`,
+          }}
+        ></div>
+        <div className="bg-gray-900 rounded-xl row-span-2 p-3 flex flex-wrap items-start content-start gap-3">
+          {techStack.map((techName) => (
+            <p className="border-2 border-gray-300 rounded-2xl px-2 py-1 bg-[#fff2]">
+              {techName}
+            </p>
+          ))}
+        </div>
+        <div className="bg-gray-900 rounded-xl px-4 py-3">
+          <p className="text-lg font-extrabold mb-4">{projectName}</p>
+          <div className="flex items-center gap-x-2 mt-2">
+            <a href={gitHubUrl} target="_blank">
+              <FaGithub className="text-3xl cursor-pointer transition-all duration-500 hover:scale-110" />
+            </a>
+            <a href={demoUrl} target="_blank">
+              <AiOutlineGlobal className="text-3xl cursor-pointer transition-all duration-500 hover:scale-110" />
+            </a>
           </div>
-        </MacScreen>
-      ))}
-    </div>
+        </div>
+      </div>
+    </MacScreen>
   )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,18 @@
 import { ProjectCard } from "../components/ProjectCard"
+import { projectsArr } from "../data/projects"
 
 export function Projects() {
   return (
-    <div>
-      <ProjectCard />
+    <div className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-y-10">
+      {projectsArr.map((item) => (
+        <ProjectCard
+          projectImageName={item.img_name}
+          techStack={item.tech_stack}
+          projectName={item.project_name}
+          gitHubUrl={item.github}
+          demoUrl={item.url}
+        />
+      ))}
     </div>
   )
 }
