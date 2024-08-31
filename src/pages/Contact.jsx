e088ea2779f418d30869a88e77e5e96c7111c151
ngan <vutran@sheridancollege.ca>
2024-08-31 13:17:04 -0400

    Form Data Management is done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,29 @@
-function FormInput({ labelText, inputType, inputId, required }) {
+import { useState } from "react"
+
+function FormInput({
+  labelText,
+  inputType,
+  inputId,
+  required,
+  handleInputChange,
+  inputName,
+  inputValue,
+  labelClassName,
+}) {
   return (
     <div className="flex flex-col gap-y-2 mb-3">
-      <label htmlFor={inputId} className="font-light text-lg">
+      <label
+        htmlFor={inputId}
+        className={`font-light text-lg ${labelClassName}`}
+      >
         {labelText} {required && "*"}
       </label>
       <input
         id={inputId}
         type={inputType}
+        onChange={handleInputChange}
+        name={inputName}
+        value={inputValue}
         className="bg-transparent px-2 py-3 border-[1px] hover:border-[2px] border-gray-300 lg:min-w-[300px]"
       />
     </div>
@@ -14,6 +31,37 @@ function FormInput({ labelText, inputType, inputId, required }) {
 }
 
 export function Contact() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    subject: "",
+    message: "",
+  })
+  const [formError, setFormError] = useState(null)
+  function handleChange(e) {
+    const { name, value, type, checked } = e.target
+    formData.email !== "" && setFormError(null)
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      }
+    })
+  }
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    formData.email == "" &&
+      setFormError("Please fill in your email to get notifications")
+    console.log(formData)
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      subject: "",
+      message: "",
+    })
+  }
   return (
     <section className="text-color md:flex md:justify-between">
       <div>
@@ -32,20 +80,47 @@ export function Contact() {
           </a>
         </div>
       </div>
-      <form className="mt-10 md:mt-0 md:grid md:grid-cols-2 md:gap-x-6">
+      <form
+        onSubmit={handleFormSubmit}
+        className="mt-10 md:mt-0 md:grid md:grid-cols-2 md:gap-x-6"
+      >
         <FormInput
           labelText="First Name"
           inputType="text"
           inputId="firstName"
+          inputName="firstName"
+          inputValue={formData.firstName}
+          handleInputChange={handleChange}
         />
-        <FormInput labelText="Last Name" inputType="text" inputId="lastName" />
+        <FormInput
+          labelText="Last Name"
+          inputType="text"
+          inputId="lastName"
+          inputName="lastName"
+          inputValue={formData.lastName}
+          handleInputChange={handleChange}
+        />
         <FormInput
           labelText="Email"
           inputType="email"
           inputId="email"
           required={true}
+          inputName="email"
+          inputValue={formData.email}
+          handleInputChange={handleChange}
+          labelClassName={
+            formError == "Please fill in your email to get notifications" &&
+            "text-red-600"
+          }
         />
-        <FormInput labelText="Subject" inputType="text" inputId="subject" />
+        <FormInput
+          labelText="Subject"
+          inputType="text"
+          inputId="subject"
+          inputName="subject"
+          inputValue={formData.subject}
+          handleInputChange={handleChange}
+        />
         <div className="flex flex-col gap-y-2 mb-3">
           <label htmlFor="message" className="font-light text-lg">
             Message
@@ -54,6 +129,9 @@ export function Contact() {
             id="message"
             rows="5"
             className="bg-transparent px-2 py-3 border-[1px] hover:border-[2px] border-gray-300 w-full"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
           ></textarea>
         </div>
         <button
@@ -62,6 +140,11 @@ export function Contact() {
         >
           Submit
         </button>
+        {formError && (
+          <p className="col-span-2 text-center mt-4 text-red-600">
+            {formError}
+          </p>
+        )}
       </form>
     </section>
   )
