eb33fec2c2ddf61a710936123a66a2d07bed14df
ngan <vutran@sheridancollege.ca>
2024-08-30 22:57:30 -0400

    Add json data of projects and render project cards based on the data added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,38 +1,84 @@
 import { MacScreen } from "../components/MacScreen"
 import { FaGithub } from "react-icons/fa"
 import { AiOutlineGlobal } from "react-icons/ai"
+import { projectsArr } from "../data/projects"
 
 export function ProjectCard() {
   return (
-    <MacScreen className="max-w-[500px] cursor-pointer transition-all duration-500 hover:scale-105">
-      <div className="grid grid-cols-2 grid-rows-2 gap-3 mt-5">
-        <div className="bg-gray-900 rounded-xl p-3 flex flex-wrap gap-x-2 justify-between">
-          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
-          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
-          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
-          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
-          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
-          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
-          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
-          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
-          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
-          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
-          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
-          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
-        </div>
-        <div className=" bg-gray-900 rounded-xl row-span-2 p-3"></div>
-        <div className="bg-gray-900 rounded-xl px-4 py-3">
-          <p className="text-lg font-extrabold mb-4">Recipe Sharing App</p>
-          <div className="flex items-center gap-x-2 mt-2">
-            <a href="https://github.com/fcbvttn123" target="_blank">
-              <FaGithub className="text-3xl cursor-pointer transition-all duration-500 hover:scale-110" />
-            </a>
-            <a href="https://www.google.ca/" target="_blank">
-              <AiOutlineGlobal className="text-3xl cursor-pointer transition-all duration-500 hover:scale-110" />
-            </a>
+    <div className="grid grid-cols-2 gap-6">
+      {projectsArr.map((item) => (
+        <MacScreen className="w-full cursor-pointer transition-all duration-500 hover:scale-105">
+          <div className="grid grid-cols-2 grid-rows-2 gap-3 mt-5">
+            <div className="bg-gray-900 rounded-xl p-3 flex flex-wrap gap-x-2 justify-between">
+              <img
+                src="./images/react-blw.png"
+                className="w-7 h-7 object-cover"
+              />
+              <img
+                src="./images/react-blw.png"
+                className="w-7 h-7 object-cover"
+              />
+              <img
+                src="./images/react-blw.png"
+                className="w-7 h-7 object-cover"
+              />
+              <img
+                src="./images/react-blw.png"
+                className="w-7 h-7 object-cover"
+              />
+              <img
+                src="./images/react-blw.png"
+                className="w-7 h-7 object-cover"
+              />
+              <img
+                src="./images/react-blw.png"
+                className="w-7 h-7 object-cover"
+              />
+              <img
+                src="./images/react-blw.png"
+                className="w-7 h-7 object-cover"
+              />
+              <img
+                src="./images/react-blw.png"
+                className="w-7 h-7 object-cover"
+              />
+              <img
+                src="./images/react-blw.png"
+                className="w-7 h-7 object-cover"
+              />
+              <img
+                src="./images/react-blw.png"
+                className="w-7 h-7 object-cover"
+              />
+              <img
+                src="./images/react-blw.png"
+                className="w-7 h-7 object-cover"
+              />
+              <img
+                src="./images/react-blw.png"
+                className="w-7 h-7 object-cover"
+              />
+            </div>
+            <div
+              className="bg-gray-900 rounded-xl row-span-2 p-3 overflow-hidden bg-cover bg-center"
+              style={{
+                backgroundImage: `url("./projects-images/${item.img_name}")`,
+              }}
+            ></div>
+            <div className="bg-gray-900 rounded-xl px-4 py-3">
+              <p className="text-lg font-extrabold mb-4">{item.project_name}</p>
+              <div className="flex items-center gap-x-2 mt-2">
+                <a href="https://github.com/fcbvttn123" target="_blank">
+                  <FaGithub className="text-3xl cursor-pointer transition-all duration-500 hover:scale-110" />
+                </a>
+                <a href={item.url} target="_blank">
+                  <AiOutlineGlobal className="text-3xl cursor-pointer transition-all duration-500 hover:scale-110" />
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </MacScreen>
+        </MacScreen>
+      ))}
+    </div>
   )
 }
---
+++ src/data/projects.js
@@ -0,0 +1,101 @@
+export const projectsArr = [
+  {
+    img_name: "blog.png",
+    project_name: "Blog Website",
+    project_desc: [
+      "Developed a blog website using React JS",
+      "Users can add, delete and read (as guests) blogs",
+      "Implemented responsive and intuitive user interfaces using HTML and Tailwind CSS",
+      "Using Google Firebase - Firestore as database for blog storage",
+      "Using Google Firebase - Authentication for google account login",
+      "Familiarity with Vite to build and bundle project for deployment",
+    ],
+    url: "https://splendid-khapse-2931da.netlify.app",
+  },
+  {
+    img_name: "chat-app.png",
+    project_name: "React Chat App",
+    project_desc: [
+      "Developed a real-time chat application using React JS",
+      "Implemented responsive and intuitive user interfaces using HTML and Tailwind CSS",
+      "Using Google Firebase as database for all messages",
+      "Familiarity with Vite to build and bundle project for deployment",
+    ],
+    url: "https://glittery-heliotrope-9b740a.netlify.app",
+  },
+  {
+    img_name: "weather-app.png",
+    project_name: "Weather App",
+    project_desc: [
+      "Developed a modern and responsive Weather App using React JS, Vite",
+      "Using Tailwind CSS and Mobile First Approach to provide an intuitive user interface across various devices",
+      "Implemented features to display today's weather, air condition, daily forecast, and hourly forecast",
+      "Managed project dependencies and package distribution using NPM",
+    ],
+    url: "https://playful-gingersnap-9bed2e.netlify.app",
+  },
+  {
+    img_name: "landing-page-coffee-shop.png",
+    project_name: "Landing Page",
+    project_desc: [
+      "Using HTML and Tailwind CSS to design the website",
+      "Utilize CSS Animation for friendly UI",
+      "Apply basic JavaScript for product slider",
+      "Integrate React and Vite to build the project",
+    ],
+    url: "https://beamish-longma-151a5d.netlify.app",
+  },
+  {
+    img_name: "minesweeper.png",
+    project_name: "Minesweeper",
+    project_desc: [
+      "Building the basic Minesweeper game on purpose of improving JavaScript skill",
+      "The game has enough basic functions such as: marking tile, mine setup, empty tile expansion,...",
+      "Using basic CSS to design the game",
+    ],
+    url: "https://chipper-heliotrope-92c1a4.netlify.app",
+  },
+  {
+    img_name: "shopping-cart.png",
+    project_name: "Shopping Cart",
+    project_desc: [
+      "Developed a Shopping Cart website which is responsive across multiple screens",
+      "Users can add, delete, update products into their shopping cart",
+      "The shopping cart is persistent across multiple tabs",
+      "Using Tailwind CSS for the Website Design and JavaScript for the Website functions",
+    ],
+    url: "https://keen-dodol-1ba301.netlify.app",
+  },
+  {
+    img_name: "quizz-app.png",
+    project_name: "Quiz App",
+    project_desc: [
+      "Building a simple Quiz App with questions about music",
+      "User can random questions and click submit to check their answers",
+      "Using React and Vite to build the whole project",
+      "Utilize Tailwind CSS for responsive web design",
+    ],
+    url: "https://coruscating-fenglisu-02f914.netlify.app",
+  },
+  {
+    img_name: "landing-page-scrimba.png",
+    project_name: "Landing Page",
+    project_desc: [
+      "The website is a result from completing the Tailwind CSS tutorial on the Internet",
+      "Responsive Web Design",
+      "Utilize Vite and React to build and deploy the website",
+    ],
+    url: "https://bespoke-haupia-9f456a.netlify.app",
+  },
+  {
+    img_name: "tenzies.png",
+    project_name: "Tenzies",
+    project_desc: [
+      "Apply Vite and React to build a basic tenzies game",
+      "The game keeps track of the number of games played",
+      "The game can calculate the average times the player rolls dice to win",
+      "The game can track the time a player need to win one game",
+    ],
+    url: "https://poetic-gnome-9d3141.netlify.app",
+  },
+]
