377bf941e1a4bb6b99c69636d5eb54709f2636c2
ngan <vutran@sheridancollege.ca>
2024-08-30 22:11:45 -0400

    Card Content Layout is done for Project Card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,14 +1,33 @@
 import { MacScreen } from "../components/MacScreen"
+import { FaGithub } from "react-icons/fa"
+import { AiOutlineGlobal } from "react-icons/ai"
 
 export function ProjectCard() {
   return (
-    <MacScreen className="max-w-[450px]">
-      <div className="mt-4 flex gap-x-4">
-        <div>
-          <div className="w-[180px] h-[180px] bg-gray-900 rounded-xl"></div>
-          <div className="w-[180px] h-[50px] bg-gray-900 rounded-xl mt-6"></div>
+    <MacScreen className="max-w-[480px]">
+      <div className="grid grid-cols-2 grid-rows-2 gap-3 mt-5">
+        <div className="bg-gray-900 rounded-xl p-3 flex flex-wrap gap-x-2 justify-between">
+          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
+          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
+          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
+          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
+          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
+          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
+          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
+          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
+          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
+          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
+          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
+          <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
         </div>
-        <div className="h-[254px] w-[100%] bg-gray-900 rounded-xl"></div>
+        <div className=" bg-gray-900 rounded-xl row-span-2 p-3"></div>
+        <div className="bg-gray-900 rounded-xl px-4 py-3">
+          <p>Project Name</p>
+          <div className="flex items-center gap-x-2 mt-2">
+            <FaGithub className="text-xl" />
+            <AiOutlineGlobal className="text-xl" />
+          </div>
+        </div>
       </div>
     </MacScreen>
   )
