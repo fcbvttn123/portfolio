c69bae9a136ef4c95fe84b8ad030c5f4c4af2613
ngan <vutran@sheridancollege.ca>
2024-08-30 22:29:25 -0400

    Card Style is done

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,7 +4,7 @@ import { AiOutlineGlobal } from "react-icons/ai"
 
 export function ProjectCard() {
   return (
-    <MacScreen className="max-w-[480px]">
+    <MacScreen className="max-w-[500px] cursor-pointer transition-all duration-500 hover:scale-105">
       <div className="grid grid-cols-2 grid-rows-2 gap-3 mt-5">
         <div className="bg-gray-900 rounded-xl p-3 flex flex-wrap gap-x-2 justify-between">
           <img src="./images/react-blw.png" className="w-7 h-7 object-cover" />
@@ -22,10 +22,14 @@ export function ProjectCard() {
         </div>
         <div className=" bg-gray-900 rounded-xl row-span-2 p-3"></div>
         <div className="bg-gray-900 rounded-xl px-4 py-3">
-          <p>Project Name</p>
+          <p className="text-lg font-extrabold mb-4">Recipe Sharing App</p>
           <div className="flex items-center gap-x-2 mt-2">
-            <FaGithub className="text-xl" />
-            <AiOutlineGlobal className="text-xl" />
+            <a href="https://github.com/fcbvttn123" target="_blank">
+              <FaGithub className="text-3xl cursor-pointer transition-all duration-500 hover:scale-110" />
+            </a>
+            <a href="https://www.google.ca/" target="_blank">
+              <AiOutlineGlobal className="text-3xl cursor-pointer transition-all duration-500 hover:scale-110" />
+            </a>
           </div>
         </div>
       </div>
