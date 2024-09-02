a9f2583301c34646848b74a44a1c5f8853285b94
ngan <vutran@sheridancollege.ca>
2024-09-02 12:12:25 -0400

    Update vertical padding for footer

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -3,12 +3,12 @@ import { FaGithub } from "react-icons/fa"
 
 export function FooterSection() {
   return (
-    <footer className="text-color pt-7 -mx-4 px-4 border-t-2 border-gray-300 md:flex md:items-center md:justify-between">
+    <footer className="text-color py-4 -mx-4 px-4 border-t-2 border-gray-300 md:flex md:items-center md:justify-between">
       <div>
         <h1 className="font-bold text-xl mb-1">Phone</h1>
         <a href="tel:+14372136600">437-213-6600</a>
       </div>
-      <div className="my-4">
+      <div className="my-4 md:my-0">
         <h1 className="font-bold text-xl mb-1">Email</h1>
         <a href="mailto:vutran@sheridancollege.ca">fcbvttn@gmail.com</a>
       </div>
